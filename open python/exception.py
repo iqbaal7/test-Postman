2c999baf4d3583e2ce742ae8f6a551c2390d4836
iqbaal7 <149063787+iqbaal7@users.noreply.github.com>
2024-02-12 17:45:35 +0100

    Delete open python directory

--- open python/exception.py
+++
@@ -1,6 +0,0 @@
-while True:
-    try:
-        x = int(input("Entrez un nombre entier : "))
-        break
-    except ValueError:
-        print("Oops ! Ce n'est pas un nombre entier. Essayez encore...")
