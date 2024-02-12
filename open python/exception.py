e7e90f321766ecd04b2e0cf26cd6de34b82fdc9c
iqbaal7 <149063787+iqbaal7@users.noreply.github.com>
2024-02-12 15:12:58 +0100

    Update exception.py

--- open python/exception.py
+++ open python/exception.py
@@ -1,1 +1,6 @@
-print("Hello, world!")
+while True:
+    try:
+        x = int(input("Entrez un nombre entier : "))
+        break
+    except ValueError:
+        print("Oops ! Ce n'est pas un nombre entier. Essayez encore...")
