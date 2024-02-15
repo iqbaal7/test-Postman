94b8972b67d26c1bcc0ec0d65d5c11f62c451258
iqbaal7 <iaa78520@gmail.com>
2024-02-15 17:02:16 +0100

    Creations des relations many to one

--- django-web-app/merchex/listings/admin.py
+++ django-web-app/merchex/listings/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+# listings/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from listings.models import Band,Listing
+
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre') 
+
+admin.site.register(Band, BandAdmin)
+admin.site.register(Listing)
--- django-web-app/merchex/listings/models.py
+++ django-web-app/merchex/listings/models.py
@@ -1,7 +1,32 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 
 # Create your models here.
 
 
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
+    genre = models.fields.CharField(max_length=100)
+    biography = models.fields.CharField(max_length=1000)
+    year_formed = models.fields.IntegerField(
+    validators=[MinValueValidator(1900), MaxValueValidator(2024)]
+    )
+    active = models.fields.BooleanField(default=True)
+    official_homepage = models.fields.URLField(null=True, blank=True)
+    def __str__(self):
+        return f'{self.name}'
+    
+class Genre(models.TextChoices):
+    HIP_HOP = 'HH'
+    SYNTH_POP = 'SP'
+    ALTERNATIVE_ROCK = 'AR' 
+genre = models.fields.CharField(choices=Genre.choices, max_length=5)
+
+class Listing(models.Model):
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+
+
+    
+    
+    
