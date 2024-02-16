5088a3358f73c2b3d9e28b44ca2c035e5bda828a
iqbaal7 <iaa78520@gmail.com>
2024-02-16 16:59:56 +0100

    Récupérez les données de l'utilisateur avec Django Forms

--- django-web-app/merchex/listings/views.py
+++ django-web-app/merchex/listings/views.py
@@ -3,10 +3,30 @@ from django.http import HttpResponse
 from listings.models import Band
 # Create your views here.
 
+from listings.forms import ContactUsForm
+
+def contact(request):
+  form = ContactUsForm()  # ajout d’un nouveau formulaire ici
+  return render(request,
+          'listings/contact.html',
+          {'form': form})
+
 def hello(request):
     bands = Band.objects.all()
     return render(request, 'listings\hello.html',
                   context={"bands": bands})
+
+def band_list(request):  # renommer la fonction de vue
+   bands = Band.objects.all()
+   return render(request,
+           'listings/band_list.html',  # pointe vers le nouveau nom de modèle
+           {'bands': bands})
+
+def band_detail(request, id):
+  band = Band.objects.get(id=id)  # nous insérons cette ligne pour obtenir le Band avec cet id
+  return render(request,
+          'listings/band_detail.html',
+          {'band': band}) # nous mettons à jour cette ligne pour passer le groupe au gabarit
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
--- django-web-app/merchex/merchex/urls.py
+++ django-web-app/merchex/merchex/urls.py
@@ -4,6 +4,11 @@ from listings import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('hello/', views.hello),
     path('about-us/', views.about),
+
+    path('bands/', views.band_list, name='band-list'),
+    path('bands/<int:id>/', views.band_detail, name='band-detail'),
+    path('contact-us/', views.contact, name='contact'),
 ]
